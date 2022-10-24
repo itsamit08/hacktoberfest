698069d48b696f7b4b5cc17b562f7986fe987a2b
itsamit08 <116475528+itsamit08@users.noreply.github.com>
2022-10-24 09:02:20 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+class addition
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=5;
+		int sum=a+b;
+		int diff=a-b;
+		System.out.println(sum);
+		System.out.println(diff);
+	}
+}	
