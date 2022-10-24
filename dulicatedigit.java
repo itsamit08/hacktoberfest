b53bacf58837e2c888f6d2c7cd6c95e3ed7f5c18
itsamit08 <116475528+itsamit08@users.noreply.github.com>
2022-10-24 09:03:45 +0530

    Add files via upload

---
+++ dulicatedigit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dulicatedigit {
+	public static void main(String args [])
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int num[]=new int[N];
+		for(int i=0;i<N;i++)
+		{
+			num[i]=sc.nextInt();
+			System.out.println(num[i]);
+		}
+		
+	}
+
+}
