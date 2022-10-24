750257828465e91941c74f8895231dd5771fadd8
itsamit08 <116475528+itsamit08@users.noreply.github.com>
2022-10-24 09:02:44 +0530

    Add files via upload

---
+++ addition2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class addition2
+{
+	public static void main(String ags[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println(sum);
+	}
+}	
